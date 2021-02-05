e2baad41bc4f8bcdba465929a625a95fa4996209
bhatinaveen <bhatinaveen335@gmail.com>
2021-02-05 13:03:15 +0530

    first commit

---
+++ spring-security-demo/src/main/java/com/java/controller/DemoClass.java
@@ -0,0 +1,16 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoClass {
+
+	@GetMapping("/")
+	public String showHome() {
+	System.out.println("hell");
+		return "home";
+	}
+	
+	
+}
